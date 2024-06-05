34f5c98a6573768313af4ae9d2b3f7cd0b4bc73d
NiltonHuamanV <niltonhuaman16@gmail.com>
2024-06-04 19:48:28 -0500

    Proyecto base con listar e insertar distritos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DistrictComponent } from './components/district/district.component';
+import { CreaeditadistrictComponent } from './components/district/creaeditadistrict/creaeditadistrict.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"district", component: DistrictComponent,
+        children:[
+            {
+                path:'insertardistrict', component:CreaeditadistrictComponent
+            }
+        ]
+    }
+];
