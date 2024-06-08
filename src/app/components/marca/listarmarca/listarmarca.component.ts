dba34de2125a9d23bcba3d2d4adc5b6dffabc62b
Felix <santosgonzales1203@gmail.com>
2024-06-07 23:19:22 -0500

    Uso de Card con solo texto

--- src/app/components/marca/listarmarca/listarmarca.component.ts
+++ src/app/components/marca/listarmarca/listarmarca.component.ts
@@ -3,19 +3,29 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Marca } from '../../../models/marca';
 import { MarcaService } from '../../../services/marca.service';
 import { RouterLink } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-listarmarca',
   standalone: true,
-  imports: [MatTableModule, RouterLink],
+  imports: [
+    MatTableModule,
+    RouterLink,
+    MatCardModule,
+    CommonModule
+  ],
   templateUrl: './listarmarca.component.html',
   styleUrl: './listarmarca.component.css'
 })
 export class ListarmarcaComponent implements OnInit{
+  marcas: Marca[] = [];
+
   displayedColumns: string[] = [
     'codigo',
     'marca'
   ];
+
   dataSource:MatTableDataSource<Marca> = new MatTableDataSource();
   constructor(private mS:MarcaService){}
 
@@ -26,5 +36,19 @@ export class ListarmarcaComponent implements OnInit{
     this.mS.getlist().subscribe(data => {
       this.dataSource = new MatTableDataSource(data)
     });
+    this.cargarMarcas();
+    this.mS.getlist().subscribe((marcas: Marca[]) => {
+      this.marcas = marcas;
+      this.dataSource = new MatTableDataSource(marcas);
+    });
   }
+
+
+  cargarMarcas(): void {
+    this.mS.list().subscribe((marcas: Marca[]) => {
+      this.marcas = marcas;
+      this.dataSource = new MatTableDataSource(marcas);
+    });
+  }
+
 }
