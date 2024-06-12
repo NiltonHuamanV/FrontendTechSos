8247d355c5f1647bb4cb93ffe2f15ba2f67c542c
Felix <santosgonzales1203@gmail.com>
2024-06-11 19:58:00 -0500

    front con seguridad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,37 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+import { NgIf } from '@angular/common';
+import { LoginService } from './services/login.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatToolbarModule,MatIconModule,MatMenuModule,MatButtonModule, RouterLink],
+  imports: [RouterOutlet,MatToolbarModule,MatIconModule,MatMenuModule,MatButtonModule, RouterLink, 
+    NgIf
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'techsos';
+
+  role: string = '';
+  constructor(private loginService: LoginService) {}
+
+  cerrar() {
+    sessionStorage.clear();
+  }
+
+  verificar() {
+    this.role = this.loginService.showRole();
+    return this.loginService.verificar();
+  }
+
+  isTecnico() {
+    return this.role == "TECNICO";
+  }
+
+  isCliente() {
+    return this.role == "CLIENTE";
+  }
 }
