045a893d55a9676015de7ee41b2bc25a80a7f095
pc GAMER <chagray28@gmail.com>
2024-06-14 22:17:39 -0500

    Marcelo Chagray - CRUD COMPLETO (Comentarios, Taller)

--- src/app/components/taller/listartaller/listartaller.component.ts
+++ src/app/components/taller/listartaller/listartaller.component.ts
@@ -2,19 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { TallerService } from '../../../services/taller.service';
 import { Taller } from '../../../models/taller';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { RouterLink } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-listartaller',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [
+    MatButtonModule,
+    MatTableModule,
+    MatFormFieldModule,
+    RouterLink,
+    MatPaginatorModule,
+    MatInputModule,
+    MatIconModule
+  ],
   templateUrl: './listartaller.component.html',
   styleUrl: './listartaller.component.css'
 })
 export class ListartallerComponent implements OnInit {
 
-  displayedColumns: String[] = ['codigo', 'taller','numerodetelefono','direccion', 'distrito'];
   dataSource:MatTableDataSource<Taller> = new MatTableDataSource()
-  constructor(private tS:TallerService) {
+  displayedColumns: String[] =
+  ['codigo',
+  'taller',
+  'numerodetelefono',
+  'direccion',
+  'distrito',
+  'accion01',
+  'accion02'];
+
+  constructor(private tS:TallerService, private snackBar:MatSnackBar) {
   }
   ngOnInit(): void {
     this.tS.list().subscribe(data=> {
@@ -24,6 +47,14 @@ export class ListartallerComponent implements OnInit {
       this.dataSource = new MatTableDataSource(data)
     })
 
+}
+
+eliminar(id: number) {
+  this.tS.eliminar(id).subscribe((data) => {
+    this.tS.list().subscribe((data) => {
+      this.tS.setlist(data);
+    });
+  });
 }
 
 }
