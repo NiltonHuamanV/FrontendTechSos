93401cb993164cc921c8df154dcb73cd3fdb752b
Felix <santosgonzales1203@gmail.com>
2024-06-16 00:46:07 -0500

    listado correcto de reparacion

--- src/app/components/reparacion/creaeditareparacion/creaeditareparacion.component.ts
+++ src/app/components/reparacion/creaeditareparacion/creaeditareparacion.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Params, Router, RouterLink } from '@angular/router';
 import { Reparacion } from '../../../models/reparacion';
 import { DispositivoTaller } from '../../../models/dispositivotaller';
 import { ReparacionService } from '../../../services/reparacion.service';
@@ -31,21 +31,33 @@ import { DispositivotallerService } from '../../../services/dispositivotaller.se
   templateUrl: './creaeditareparacion.component.html',
   styleUrl: './creaeditareparacion.component.css'
 })
-export class CreaeditareparacionComponent {
+export class CreaeditareparacionComponent implements OnInit{
 
   form: FormGroup = new FormGroup({});
   reparation: Reparacion = new Reparacion();
   listaReparaciones: Reparacion[] = [];
   listaDispositivos: DispositivoTaller[] = [];
 
+  id:number = 0;
+  edicion: boolean=false;
+
   constructor(private formBuilder:FormBuilder,
     private rS:ReparacionService,
     private dtS:DispositivotallerService,
-    private router:Router
+    private router:Router,
+    private route:ActivatedRoute,
   ){}
 
   ngOnInit(): void {
+    this.route.params.subscribe((data: Params) => {
+      this.id = data['id'];
+      this.edicion = data['id'] != null;
+      this.init();
+    });
+
+
     this.form=this.formBuilder.group({
+      codigo:[''],
       dispositivotaller:['',Validators.required],
       fechainicio:['',Validators.required],
       fechafin:['',Validators.required],
@@ -60,22 +72,53 @@ export class CreaeditareparacionComponent {
 
   aceptar():void {
     if(this.form.valid) {
+      this.reparation.idReparacion = this.form.value.codigo;
       this.reparation.dispositivoTaller.idDispositivoTaller = this.form.value.dispositivotaller;
-      this.reparation.FechaInicio = this.form.value.fechainicio;
-      this.reparation.FechaFin = this.form.value.fechafin;
-      this.reparation.Problema = this.form.value.problema;
-      this.reparation.Estado = this.form.value.estado;
-      this.reparation.Costo = this.form.value.costo;
+      this.reparation.fechaInicio = this.form.value.fechainicio;
+      this.reparation.fechaFin = this.form.value.fechafin;
+      this.reparation.problema = this.form.value.problema;
+      this.reparation.estado = this.form.value.estado;
+      this.reparation.costo = this.form.value.costo;
 
-      this.rS.insert(this.reparation).subscribe((data)=>{
+      if (this.edicion) {
+        this.rS.update(this.reparation).subscribe((data) => {
+          this.rS.list().subscribe((data) => {
+            this.rS.setList(data);
+          });
+        });
+      }else{
+      this.rS.insert(this.reparation).subscribe((data) => {
+        this.rS.list().subscribe((data) => {
+          this.rS.setList(data);
+        });
+      });
+      }
+      /*this.rS.insert(this.reparation).subscribe((data)=>{
         this.rS.list().subscribe((data)=>{
           this.rS.setList(data)
         })
-      });
+      });*/
 
       this.router.navigate(['reparacion'])
     }
   }
 
+
+  init(){
+    if (this.edicion)
+      {
+        this.rS.listId(this.id).subscribe((data)=> {
+          this.form = new FormGroup({
+            codigo: new FormControl(data.idReparacion),
+            dispositivotaller: new FormControl(data.dispositivoTaller.idDispositivoTaller),
+            fechainicio: new FormControl(data.fechaInicio),
+            fechafin: new FormControl(data.fechaFin),
+            problema: new FormControl(data.problema),
+            estado: new FormControl(data.estado),
+            costo: new FormControl(data.costo),
+          })
+        })
+      }
+  }
 
 }
--- src/app/models/reparacion.ts
+++ src/app/models/reparacion.ts
@@ -2,11 +2,11 @@ import { DispositivoTaller } from "./dispositivotaller";
 
 export class Reparacion{
     idReparacion: number = 0;
-    FechaInicio: Date=new Date(Date.now())
-    FechaFin: Date=new Date(Date.now())
-    Problema: string = ""
-    Estado: string = ""
-    Costo: number = 0
+    fechaInicio: Date=new Date(Date.now())
+    fechaFin: Date=new Date(Date.now())
+    problema: string = ""
+    estado: string = ""
+    costo: number = 0
     dispositivoTaller: DispositivoTaller = new DispositivoTaller()
 
 }
