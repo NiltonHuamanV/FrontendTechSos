7145fb252628a16d1b683840c595ab62710993a3
NiltonHuamanV <niltonhuaman16@gmail.com>
2024-06-19 17:20:02 -0500

    Diseño css para el login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -4,14 +4,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { LoginService } from '../../services/login.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { JwtRequest } from '../../models/jwtRequest';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatButtonModule, MatInputModule],
+  imports: [FormsModule, MatFormFieldModule, MatButtonModule, MatInputModule,RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -15,18 +15,24 @@ export class LoginService {
   }
 
   verificar() {
-    let token = sessionStorage.getItem('token');
-    return token != null;
+    if (typeof window !== 'undefined' && typeof window.sessionStorage !== 'undefined') {
+      let token = sessionStorage.getItem('token');
+      return token != null;
+    }
+    return false;
   }
 
   showRole() {
-    let token = sessionStorage.getItem('token');
-    if (!token) {
-      // Manejar el caso en el que el token es nulo.
-      return null; // O cualquier otro valor predeterminado dependiendo del contexto.
+    if (typeof window !== 'undefined' && typeof window.sessionStorage !== 'undefined') {
+      let token = sessionStorage.getItem('token');
+      if (!token) {
+        // Manejar el caso en el que el token es nulo.
+        return null; // O cualquier otro valor predeterminado dependiendo del contexto.
+      }
+      const helper = new JwtHelperService();
+      const decodedToken = helper.decodeToken(token);
+      return decodedToken?.role;
     }
-    const helper = new JwtHelperService();
-    const decodedToken = helper.decodeToken(token);
-    return decodedToken?.role;
+    return null; // O cualquier otro valor predeterminado dependiendo del contexto.
   }
 }
